74ee3db691c01429d528668d0724d08d329c4544
Xiaonanhai <86953256+mrbeizi@users.noreply.github.com>
2025-05-23 16:56:39 +0700

    [setting] auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         if(Auth::guard('karyawan')->attempt(['nik' => $request->nik,'password' => $request->password])){
             return Redirect::route('dashboard');
         }else{
-            return Redirect::route('login')->with(['error' => 'NIK / Password salah.']);
+            return Redirect::route('login')->with(['error' => 'NIK atau Password salah.']);
         }
 
     }
@@ -25,5 +25,23 @@ class AuthController extends Controller
             Auth::guard('karyawan')->logout();
             return redirect('/');
         }
+    }
+
+    public function prosesLogoutAdmin()
+    {
+        if(Auth::guard('user')->check()){
+            Auth::guard('user')->logout();
+            return redirect('/panel');
+        }
+    }
+
+    public function prosesLoginAdmin(Request $request)
+    {
+        if(Auth::guard('user')->attempt(['email' => $request->email,'password' => $request->password])){
+            return Redirect::route('dashboardadmin');
+        }else{
+            return Redirect::route('loginadmin')->with(['warning' => 'Email atau Password salah.']);
+        }
+
     }
 }
